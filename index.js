c7452529fd2799406a192f78dc19e22969a09caf
Francisco Perez <Franciscop693@gmail.com>
2023-04-05 07:45:46 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Code your solution here
+
+function findMatching(array, string){
+    return array.filter(array => array.toLowerCase() === string.toLowerCase())
+}
+
+function fuzzyMatch(array, string){
+    return array.filter(array => array[0] === string[0])
+}
+
+function matchName(drivers, string){
+    return drivers.filter(drivers => drivers.name === string)
+}
